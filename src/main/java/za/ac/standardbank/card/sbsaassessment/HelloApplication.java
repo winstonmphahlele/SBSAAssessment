8d7fbd7d3d59dd4d7ea4f9a64263dcda4212f682
winston mphahlele <winston.mphahlele@gmail.com>
2024-10-03 11:35:29 +0200

    Initial commit

---
+++ src/main/java/za/ac/standardbank/card/sbsaassessment/HelloApplication.java
@@ -0,0 +1,9 @@
+package za.ac.standardbank.card.sbsaassessment;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
