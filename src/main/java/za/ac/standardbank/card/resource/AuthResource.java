06073e4a449fd4a5498729e1f47cf04a1db4d30b
winston mphahlele <winston.mphahlele@gmail.com>
2024-10-05 04:17:21 +0200

    move auth

--- src/main/java/za/ac/standardbank/card/resource/AuthResource.java
+++ src/main/java/za/ac/standardbank/card/resource/AuthResource.java
@@ -7,7 +7,9 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import za.ac.standardbank.card.security.util.JwtUtil;
+import za.ac.standardbank.card.util.JwtUtil;
+import za.ac.standardbank.generated.LoginRequest;
+import za.ac.standardbank.generated.LoginResponse;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,51 +21,24 @@ public class AuthResource {
     @Path("/login")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response login(UserCredentials credentials) {
-        // In a real application, you should validate the credentials with a user database
+    public Response login(LoginRequest credentials) {
+
+        //WM: need to auth against db
+
         if ("user".equals(credentials.getUsername()) && "password".equals(credentials.getPassword())) {
             String token = JwtUtil.generateToken(credentials.getUsername(), new ArrayList<>(Arrays.asList("user")));
-            return Response.ok(new AuthResponse(token)).build();
+            LoginResponse response = new LoginResponse();
+            response.setToken(token);
+            return Response.ok(response).build();
         }
 
         if ("admin".equals(credentials.getUsername()) && "password".equals(credentials.getPassword())) {
             String token = JwtUtil.generateToken(credentials.getUsername(), new ArrayList<>(Arrays.asList("admin")));
-            return Response.ok(new AuthResponse(token)).build();
+            LoginResponse response = new LoginResponse();
+            response.setToken(token);
+            return Response.ok(response).build();
         }
         return Response.status(Response.Status.UNAUTHORIZED).build();
     }
 
-    public static class UserCredentials {
-        private String username;
-        private String password;
-
-        // Getters and Setters
-        public String getUsername() {
-            return username;
-        }
-
-        public void setUsername(String username) {
-            this.username = username;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
-        }
-    }
-
-    public static class AuthResponse {
-        private String token;
-
-        public AuthResponse(String token) {
-            this.token = token;
-        }
-
-        public String getToken() {
-            return token;
-        }
-    }
 }
